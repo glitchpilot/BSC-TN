95c2280e6db385e61628bdc1cf573f64bc511f80
Glitchpilot <35514699+glitchpilot@users.noreply.github.com>
2023-10-03 00:28:45 +0800

    Update Token.sol

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,1 +1,11 @@
-asd
+// Token.sol - BEP-20 Token contract
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Token is ERC20 {
+    constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) {
+        _mint(msg.sender, 1000000 * 10 ** uint256(decimals()));
+    }
+}
